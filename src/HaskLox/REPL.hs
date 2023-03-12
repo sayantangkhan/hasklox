ab462bd64aa08a0bad196e81d0a355ea3c4be2d9
Sayantan Khan <sayantangkhan@gmail.com>
2023-03-12 11:42:08 -0400

    Added haskeline for better REPL

--- src/HaskLox/REPL.hs
+++ src/HaskLox/REPL.hs
@@ -1,50 +1,44 @@
 module HaskLox.REPL (runPrompt) where
 
-import Control.Monad (unless)
+import Control.Monad.IO.Class (liftIO)
 import Data.ByteString qualified as B
 import Data.ByteString.Lazy (fromStrict)
 import Data.Foldable (forM_)
+import Data.String qualified as B
 import HaskLox.Interpreter (evalProgram, runInterpreter)
 import HaskLox.Interpreter.Environment (Environment, initializeEnvironment)
 import HaskLox.Interpreter.Values (Value)
 import HaskLox.Parser (parseLox)
 import HaskLox.Scanner (ausEOFfound, runAlex)
-import System.IO (hFlush, isEOF, stdout)
+import System.Console.Haskeline qualified as HL
 
-runPromptSingleLine :: Environment Value -> IO ()
+runPromptSingleLine :: Environment Value -> HL.InputT IO ()
 runPromptSingleLine environment = do
-  B.putStr "> "
-  hFlush stdout
-  eof <- isEOF
-  unless eof $ do
-    line <- B.getLine
-    (foundEOF, errorMessage) <- tryRun line environment
-    if foundEOF
-      then runPromptMultiLine environment line
-      else do
-        forM_ errorMessage putStrLn
-        runPromptSingleLine environment
+  minput <- HL.getInputLine "> "
+  case minput of
+    Just line -> do
+      (foundEOF, errorMessage) <- liftIO $ tryRun (B.fromString line) environment
+      if foundEOF
+        then runPromptMultiLine environment (B.fromString line)
+        else do
+          forM_ errorMessage HL.outputStrLn
+          runPromptSingleLine environment
+    Nothing -> return ()
 
-runPromptMultiLine :: Environment Value -> B.ByteString -> IO ()
-runPromptMultiLine environment prevLines = do
-  B.putStr ": "
-  hFlush stdout
-  eof <- isEOF
-  unless eof $ do
-    line <- B.getLine
-    let lineIsNewline = line == ""
-    if lineIsNewline
-      then do
-        (_, errorMessage) <- tryRun prevLines environment
-        forM_ errorMessage putStrLn
-        runPromptSingleLine environment
-      else runPromptMultiLine environment (prevLines `B.append` "\n" `B.append` line)
-    return ()
+runPromptMultiLine :: Environment Value -> B.ByteString -> HL.InputT IO ()
+runPromptMultiLine environment prevlines = do
+  minput <- HL.getInputLine ": "
+  case minput of
+    Just line -> runPromptMultiLine environment (prevlines `B.append` "\n" `B.append` B.fromString line)
+    Nothing -> do
+      (_, errorMessage) <- liftIO $ tryRun prevlines environment
+      forM_ errorMessage HL.outputStrLn
+      runPromptSingleLine environment
 
 runPrompt :: IO ()
 runPrompt = do
   environment <- initializeEnvironment
-  runPromptSingleLine environment
+  HL.runInputT HL.defaultSettings $ runPromptSingleLine environment
 
 -- tryRun tries to evaluate the input line, and returns (True, Just errorMessage) if the line failed to parse because of an early EOF, and (False, None) if it parses successfully, and (False, Just errorMessage) due to some other error
 tryRun :: B.ByteString -> Environment Value -> IO (Bool, Maybe String)
